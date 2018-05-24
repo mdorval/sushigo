52340d2519ed594060c825f017392ca7cf4f77f8
Michael Dorval <michael.t.dorval@gmail.com>
2018-05-24 13:03:55 -0700

    Use of CardInfo and DeckInfo as serialized objects

---
+++ Assets/Scripts/CardInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardInfo  {
+    public CardType type;
+    public int copiesInDeck = 0;
+    public Sprite cardSprite;
+}
---
+++ Assets/Scripts/DeckInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="DeckInfo", menuName = "DeckInfo", order = 1)]
+public class DeckInfo : ScriptableObject {
+    public List<CardInfo> cards;
+    public CardInfo byType(CardType type)
+    {
+        foreach(CardInfo card in cards)
+        {
+            if (card.type == type)
+                return card;
+        }
+        return null;
+    }
+}

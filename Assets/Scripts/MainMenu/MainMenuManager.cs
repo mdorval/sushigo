01eb1afca930efe7e3b5003f392995403debf549
Michael Dorval <michael.t.dorval@gmail.com>
2018-05-29 12:01:49 -0700

    Added Start Screen

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuManager : MonoBehaviour {
+    public Button PlayButton;
+    public Button RulesButton;
+    public Button AboutButton;
+    public Button QuitButton;
+    public List<Button> BackToMenuButtons;
+
+    private GameObject CurrentPanel;
+    private GameObject MainPanel;
+    public GameObject AboutPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        MainPanel = this.gameObject;
+        CurrentPanel = MainPanel;
+        QuitButton.onClick.AddListener(Application.Quit);
+        PlayButton.onClick.AddListener(LoadGame);
+        AboutButton.onClick.AddListener(LoadAboutPanel);
+        foreach (Button backButton in BackToMenuButtons)
+        {
+            backButton.onClick.AddListener(LoadMainPanel);
+        }
+
+	}
+
+    private void LoadPanel(GameObject panelToLoad)
+    {
+        CurrentPanel.SetActive(false);
+        panelToLoad.SetActive(true);
+        CurrentPanel = panelToLoad;
+    }
+    void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadMainPanel() { LoadPanel(MainPanel); }
+    void LoadAboutPanel() { LoadPanel(AboutPanel); }
+}
